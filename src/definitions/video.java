8ac1937d61b61227177e10e1ac8d425fcb14c70c
Mu613 <muskan.bhardwaj_cs19@gla.ac.in>
2020-08-22 23:01:36 +0530

    add fields for the video.java class

---
+++ src/definitions/video.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Muskan Bhardwaj
+ *   Date: 22-08-2020
+ *   Time: 10:54 PM
+ *   File: video.java
+ */
+
+package definitions;
+
+public class video {
+    /*
+     *Creating Definition Class:(Rules)
+     * 1.Always set the visibility modifier of a field to private  unless otherwise specified.
+     * 2.Always set the access modifier of a field to non-static  unless otherwise specified.
+     */
+
+    //member variables =>fields/variables
+    private String videoName;
+    private boolean checkOur;
+    private int rating;
+
+}
